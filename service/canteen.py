3879099e0bb85e339f6080259df0b17d0c15f4e3
合明 <xiaoteng.fxt@antgroup.com>
2025-05-13 16:23:21 +0800

    Revert "删除锁进"
    This reverts commit 41f5cfc8c5a6102f3d12191fe3a1f8f0d88c6682.

--- service/canteen.py
+++ service/canteen.py
@@ -4,45 +4,45 @@ from service.user.base import BaseUser, UserType
 
 
 class Canteen:
-def __init__(self, name):
-self.name = name
+    def __init__(self, name):
+        self.name = name
 
-def order_food(self, user: BaseUser, foods: list[str]) -> Optional[float]:
-'''
-Students or teachers order lunch boxes in the meal ordering system
-:return:
-'''
-cookbook = None
-if user.is_student():
-cookbook = self.query_menu(user.type, discount=user.get_discount())
-elif user.is_teacher():
-cookbook = self.query_menu(user.type, discount=user.get_discount())
-if cookbook is None:
-return None
+    def order_food(self, user: BaseUser, foods: list[str]) -> Optional[float]:
+        '''
+        Students or teachers order lunch boxes in the meal ordering system
+        :return:
+        '''
+        cookbook = None
+        if user.is_student():
+            cookbook = self.query_menu(user.type, discount=user.get_discount())
+        elif user.is_teacher():
+            cookbook = self.query_menu(user.type, discount=user.get_discount())
+        if cookbook is None:
+            return None
 
-price = 0.0
-for food in foods:
-if food in cookbook:
-price += cookbook[food]
+        price = 0.0
+        for food in foods:
+            if food in cookbook:
+                price += cookbook[food]
 
-if user.card.balance_inquiry() >= price:
-user.card.deduction(price)
-return price
+        if user.card.balance_inquiry() >= price:
+            user.card.deduction(price)
+            return price
 
-return None
+        return None
 
-def query_menu(self, customer_type: UserType, discount: float = 1.0):
-if customer_type == UserType.Student:
-return {
-'noodles with sesame paste & pea sprouts': 5 * discount,
-'cool braised noodles': 7 * discount,
-'steamed glutinous rice with eight treasures': 3 * discount,
-}
-elif customer_type == UserType.Teacher:
-return {
-'noodles with sesame paste & pea sprouts': 5 * discount,
-'steamed meat dumpling': 2 * discount,
-'lightly fried Chinese bread': 3 * discount,
-}
-else:
-return None
+    def query_menu(self, customer_type: UserType, discount: float = 1.0):
+        if customer_type == UserType.Student:
+            return {
+                'noodles with sesame paste & pea sprouts': 5 * discount,
+                'cool braised noodles': 7 * discount,
+                'steamed glutinous rice with eight treasures': 3 * discount,
+            }
+        elif customer_type == UserType.Teacher:
+            return {
+                'noodles with sesame paste & pea sprouts': 5 * discount,
+                'steamed meat dumpling': 2 * discount,
+                'lightly fried Chinese bread': 3 * discount,
+            }
+        else:
+            return None
--- service/user/base.py
+++ service/user/base.py
@@ -4,24 +4,24 @@ from service.finance import CampusCard
 
 
 class UserType(Enum):
-Student = 'student'
-Teacher = 'teacher'
+    Student = 'student'
+    Teacher = 'teacher'
 
 
 class BaseUser:
-def __init__(self, id: int):
-self.id = id
-self.card = CampusCard()
-if self.is_student():
-self.type = UserType.Student
-elif self.is_teacher():
-self.type = UserType.Teacher
+    def __init__(self, id: int):
+        self.id = id
+        self.card = CampusCard()
+        if self.is_student():
+            self.type = UserType.Student
+        elif self.is_teacher():
+            self.type = UserType.Teacher
 
-def get_discount(self):
-raise NotImplementedError
+    def get_discount(self):
+        raise NotImplementedError
 
-def is_student(self):
-return self.id > 10000
+    def is_student(self):
+        return self.id > 10000
 
-def is_teacher(self):
-return self.id < 1000
+    def is_teacher(self):
+        return self.id < 1000
