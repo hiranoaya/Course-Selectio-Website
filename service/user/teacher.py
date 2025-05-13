2be66e7d8cfa69ce6bf6681e5132bcd56e4f6c2a
合明 <xiaoteng.fxt@antgroup.com>
2025-05-13 15:08:32 +0800

    The office is used for teachers to rest

---
+++ service/user/teacher.py
@@ -0,0 +1,9 @@
+from service.user.base import BaseUser
+
+
+class Teacher(BaseUser):
+    def __init__(self, id: int):
+        super().__init__(id)
+
+    def get_discount(self):
+        return 0.9
